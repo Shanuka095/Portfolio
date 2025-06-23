14fc2157d0eb53e72bcae571af3e50d3bfb92943
Shanuka Induran <shanukainduran@gmail.com>
2025-06-23 13:40:55 +0530

    Create Home.jsx

---
+++ Portfolio/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <section id="home" className="my-10">
+      <h1 className="text-4xl font-bold mb-4">Hetti Arachchige Shanuka Induran Madurapperuma</h1>
+      <p className="text-lg max-w-xl">
+        I’m Shanuka Induran, a passionate Information Technology student specializing in Software Engineering at SLIIT. I enjoy building innovative software solutions using modern technologies like MERN stack and exploring the intersection of technology and creativity.
+      </p>
+    </section>
+  );
+}
+
+export default Home;

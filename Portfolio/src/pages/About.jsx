a911225d9367207aa6aeaab41c0d2d2b2b5c5d19
Shanuka Induran <shanukainduran@gmail.com>
2025-06-23 13:43:21 +0530

    Create About.jsx

---
+++ Portfolio/src/pages/About.jsx
@@ -0,0 +1,12 @@
+function About() {
+  return (
+    <section id="about" className="my-10">
+      <h2 className="text-3xl font-bold mb-4">About Me</h2>
+      <p>BSc Honours IT (Software Engineering), SLIIT, Expected 2026.</p>
+      <p>Previously completed Higher National Diploma (HND) in IT.</p>
+      <p>Skills: JavaScript, React, Node.js, MERN stack, etc.</p>
+      {/* Add more info as needed */}
+    </section>
+  );
+}
+export default About;

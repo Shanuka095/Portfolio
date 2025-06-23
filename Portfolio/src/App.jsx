0d50965371f153ed3d77b2a358f6efc0c80bd87f
Shanuka Induran <shanukainduran@gmail.com>
2025-06-23 15:14:17 +0530

    Create App.jsx

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import Footer from "./components/Footer";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+    });
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <main className="space-y-20 px-4 py-8 md:px-8 lg:px-16 text-light-text dark:text-dark-text transition-colors duration-500">
+        <Home />
+        <About />
+        <Services />
+        <Portfolio />
+        <Contact />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
